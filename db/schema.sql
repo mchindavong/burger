666204dca15a7f7761b8d9cb2fbddaf858c41245
Mollie <molliechindavong@gmail.com>
2017-11-09 23:20:34 -0500

    test

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+USE oux99c41xcjbyg7z;
+
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
