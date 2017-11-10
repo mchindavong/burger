a7143964799ffbacc8ebf732322c6b62f6500ac7
Mollie <molliechindavong@gmail.com>
2017-11-10 13:42:16 -0500

    updating

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
     } else {
         connection = mysql.createConnection({
-            port: 3000
+            root: 3000,
             host:'localhost',
             user:'root',
             password:'root',
